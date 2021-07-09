33e3f840be79b1033605d16642979c179ef2b3af
Roderick Quiel <46735370+stevenDcat@users.noreply.github.com>
2021-07-09 01:41:22 -0500

    Updated Jul 9

--- Player.java
+++ Player.java
@@ -1,6 +1,17 @@
-
-//still don't know what we would need to implement here
+//still don't know what more would need to be implemented here
 public class Player
 {
+    private final int score;
+    private final PlayerRack playerRack;
+    public Player(LetterBag letterBag)
+    {
+        this.playerRack = new PlayerRack(letterBag);
+        this.score = 0;
+    }
+
+    public PlayerRack getPlayerRack()
+    {
+        return playerRack;
+    }
 
 }
